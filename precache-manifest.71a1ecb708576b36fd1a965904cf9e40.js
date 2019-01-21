12c50d68a29b5bc1145cfe00dfc2fdbb869a020f
Jaewoo Kim <jaewookim@host-4-197.ilcujsm.champaign.il.us.clients.pavlovmedia.net>
2019-01-21 15:07:54 -0600

    Updates

---
+++ precache-manifest.71a1ecb708576b36fd1a965904cf9e40.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6edb2139d722a1150771",
+    "url": "/axieauction/static/js/runtime~main.6edb2139.js"
+  },
+  {
+    "revision": "6306fa4e0a4023cc2a66",
+    "url": "/axieauction/static/js/main.6306fa4e.chunk.js"
+  },
+  {
+    "revision": "792fa3eca75bab06e188",
+    "url": "/axieauction/static/js/1.792fa3ec.chunk.js"
+  },
+  {
+    "revision": "6306fa4e0a4023cc2a66",
+    "url": "/axieauction/static/css/main.5b8bd14a.chunk.css"
+  },
+  {
+    "revision": "0e698a1b5131e43967eb2d9b76a8983b",
+    "url": "/axieauction/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.bedf9f6eed2c90f526887c32a2a7f024.js"
+  "/axieauction/precache-manifest.71a1ecb708576b36fd1a965904cf9e40.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/axieauction/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
